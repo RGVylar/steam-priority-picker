ac8c68c580c1ce9bf7cf7550a928e4a46a0f0d16
RGVylar <rgvylar@gmail.com>
2025-11-23 19:54:14 +0100

    feat: update GameCard component to display unknown playtime when HLTB hours are not available; add translation for unknown time

--- web/src/components/GameCard.jsx
+++ web/src/components/GameCard.jsx
@@ -81,11 +81,9 @@ export function GameCard({ game, isPlayed, onTogglePlayed, onMouseEnter, onMouse
           <p className="text-xs text-gray-600 dark:text-gray-400">
             {t('games.playtime')}: <span className="font-medium text-gray-900 dark:text-white">{game.playtime_hours.toFixed(1)} hrs</span>
           </p>
-          {game.hltb_hours > 0 && (
-            <p className="text-xs text-gray-600 dark:text-gray-400">
-              {t('games.timeToBeat')}: <span className="font-medium text-gray-900 dark:text-white">{game.hltb_hours.toFixed(1)} hrs</span>
-            </p>
-          )}
+          <p className="text-xs text-gray-600 dark:text-gray-400">
+            {t('games.timeToBeat')}: <span className="font-medium text-gray-900 dark:text-white">{game.hltb_hours > 0 ? `${game.hltb_hours.toFixed(1)} hrs` : t('games.unknown')}</span>
+          </p>
         </div>
       </div>
 
